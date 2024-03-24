38e643130ce248b6785bf58b061277028c5b6dea
sibylassana95 <sibyamara95@gmail.com>
2024-03-24 00:50:36 +0000

    update produit

--- src/app/views/produits/produits.module.ts
+++ src/app/views/produits/produits.module.ts
@@ -1,12 +1,45 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {
+  AvatarModule,
+  ButtonGroupModule,
+  ButtonModule,
+  CardModule,
+  FormModule,
+  GridModule,
+  NavModule,
+  ProgressModule,
+  TableModule,
+  TabsModule,
+} from '@coreui/angular';
 import { ProduitsRoutingModule } from './produits-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProduitsComponent } from './produits.component';
-
+import { IconModule } from '@coreui/icons-angular';
+import { ChartjsModule } from '@coreui/angular-chartjs';
+import { WidgetsModule } from '../widgets/widgets.module';
 @NgModule({
   declarations: [ProduitsComponent],
-  imports: [CommonModule, ProduitsRoutingModule, FormsModule],
+  imports: [
+    CommonModule,
+    ProduitsRoutingModule,
+    FormsModule,
+    CardModule,
+    NavModule,
+    IconModule,
+    TabsModule,
+    CommonModule,
+    GridModule,
+    ProgressModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    FormModule,
+    ButtonModule,
+    ButtonGroupModule,
+    ChartjsModule,
+    AvatarModule,
+    TableModule,
+    WidgetsModule,
+  ],
 })
 export class ProduitsModule {}
